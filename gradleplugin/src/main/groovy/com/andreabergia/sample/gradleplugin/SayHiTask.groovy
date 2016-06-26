87edee15ab6500377872b4ee20fd753cff29cf9e
Andrea Bergia <andreabergia@gmail.com>
2016-06-26 19:42:44 +0200

    Allow the task's message to be configured

--- gradleplugin/src/main/groovy/com/andreabergia/sample/gradleplugin/SayHiTask.groovy
+++ gradleplugin/src/main/groovy/com/andreabergia/sample/gradleplugin/SayHiTask.groovy
@@ -4,8 +4,10 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class SayHiTask extends DefaultTask {
+    String message = "Hello from our task"
+
     @TaskAction
     def sayHi() {
-        println "Hello from our task"
+        println message
     }
 }
