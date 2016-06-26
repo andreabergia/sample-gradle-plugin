99c82f30a0c3543c49e59b3ddd5ccb5044b711ff
Andrea Bergia <andreabergia@gmail.com>
2016-06-26 19:38:28 +0200

    Creating a task class

--- gradleplugin/src/main/groovy/com/andreabergia/sample/gradleplugin/SamplePlugin.groovy
+++ gradleplugin/src/main/groovy/com/andreabergia/sample/gradleplugin/SamplePlugin.groovy
@@ -5,9 +5,7 @@ import org.gradle.api.Project
 
 class SamplePlugin implements Plugin<Project> {
     void apply(Project project) {
-        // Define a new task
-        project.task('sample-task') << {
-            println "Hello from our sample plugin!"
-        }
+        // Define a new task of the correct type
+        project.task('sayHi', type: SayHiTask)
     }
 }
---
+++ gradleplugin/src/main/groovy/com/andreabergia/sample/gradleplugin/SayHiTask.groovy
@@ -0,0 +1,11 @@
+package com.andreabergia.sample.gradleplugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class SayHiTask extends DefaultTask {
+    @TaskAction
+    def sayHi() {
+        println "Hello from our task"
+    }
+}
